9850a2769477c56a260f63f8b11e53cc8d3eee0b
junhanliu <junhanliu.leon@gmail.com>
2019-12-09 16:47:20 +1100

    created class to implement connection with mongodb

--- ZomSpider/StandaloneOps/MongodbFeeder.py
+++ ZomSpider/StandaloneOps/MongodbFeeder.py
@@ -1,7 +1,11 @@
 import pymongo
 
-client = pymongo.MongoClient("mongodb://192.168.150:27017")
+client = pymongo.MongoClient("mongodb://192.168.86.150:27017")
 
-restaurant_db = client["restaurant"]
+restaurant_db = client["zom_proj"]
+
+restaurant_collection = restaurant_db["restaurant"]
+
+restaurant_collection.insert_one({"test":"tt"})
 
 print(client.list_database_names())
